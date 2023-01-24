8f58d049a40c4d58b790ab4bfce02cb737238cb1
JJJ4032002 <rp4888888@gmail.com>
2023-01-24 23:21:33 +0530

    Add Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
+import tokenReducer from "../features/token/tokenSlice";
+import queryReducer from "../features/query/querySlice";
+import filterReducer from "../features/filter/filterSlice";
+import viewReducer from "../features/view/viewSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    token: tokenReducer,
+    query: queryReducer,
+    filter: filterReducer,
+    view: viewReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
